9c3bea135c7292fc7a5ab5f5f55822c5bedd8023
Naganathan <nathan.mandytech@gmail.com>
2024-12-04 22:41:48 +0530

    update the utility menu style

---
+++ wp-content/themes/mortageoffice/parts/header-search.php
@@ -0,0 +1,7 @@
+<div class="header-search">
+	<div class="search-form-wrapper">
+		<?php echo get_search_form(); ?>
+	</div>
+
+	<button class="search-toggle">Search<span class="screen-reader-text"></span></button>
+</div>
---
+++ wp-content/themes/mortageoffice/src/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const node = document.querySelector(".header-search"); 
+    const textnode = document.querySelector(".utility-menu-wrapper"); 
+    if (node && textnode) {
+      node.appendChild(textnode); 
+    }
+  });
+  
