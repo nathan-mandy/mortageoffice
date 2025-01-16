741e51a05ef26efbd35660f0d17a12685287425c
Naganathan <nathan.mandytech@gmail.com>
2025-01-17 00:06:36 +0530

    Fixing the header style

--- wp-content/themes/mortageoffice/src/main.js
+++ wp-content/themes/mortageoffice/src/main.js
@@ -1,4 +1,3 @@
-import './scripts/mobile-navigation';
 import './scripts/utility-menu';
 import './scripts/blocks/roi-calculator';
 import './scripts/blocks/platform-product-card';
--- wp-content/themes/mortageoffice/src/scripts/utility-menu.js
+++ wp-content/themes/mortageoffice/src/scripts/utility-menu.js
@@ -1,7 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const node = document.querySelector(".header-search"); 
-    const textnode = document.querySelector(".utility-menu-wrapper"); 
-    if (node && textnode) {
-      node.appendChild(textnode); 
+const SELECTOR = '.header-search';
+
+function onDocumentReady() {
+	const els = document.querySelectorAll(SELECTOR);
+
+	if (!els) {
+		return;
+	}
+
+	const windowWidth = window.innerWidth;
+
+	if (windowWidth > 1200) {
+		els.forEach(e => initListener(e));
     }
-  });
+}
+
+function initListener(el) {
+	const input = el.querySelector('.search-field');
+
+	input.addEventListener('focus', (e) => {
+		activateSearch(e, el);
+	});
+
+	input.addEventListener('focusout', (e) => {
+		deactivateSearch(e, el);
+	});
+}
+
+function activateSearch(e, el) {
+	el.classList.add('activated');
+}
+
+function deactivateSearch(e, el) {
+	el.classList.remove('activated');
+}
+
+document.addEventListener('DOMContentLoaded', onDocumentReady);
